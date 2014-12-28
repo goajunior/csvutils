101663c9d22762cb2da6f51230d9519e210a52a5
George Ainsworth <georgeainsworthjr@gmail.com>
2014-12-27 23:37:19 -0200

    now we have a conversion from latin1 to utf8

--- csvp.py
+++ csvp.py
@@ -7,9 +7,12 @@ import csv
 import argparse
 import codecs
 
-def changecodec(fin):
-    
+def changecodec(fin,fileout):
 
+    fout   = codecs.open(fileout, 'w', encoding='utf-8')
+
+    for line in fin:
+        fout.write(line)
 
     return fout
 
@@ -18,21 +21,29 @@ def main():
     # opcoes para linha de comando
     parser = argparse.ArgumentParser(usage=__doc__)
     parser.add_argument("--fin", default="input.csv", help="arquivo de entrada csv")
+    parser.add_argument("--convert",  default="output-utf8.csv", help="arquivo convertido para utf8")
     parser.add_argument("--output",  default="output.txt", help="arquivo de saida")
     args = parser.parse_args()
 
     filein = open(args.fin, encoding='latin1')
-    fout = changecodec(filein)
-    print(args.fout)
-    csv_file = csv.reader(fout, dialect='excel')
 
+#:TODO: 27.12.14 23:31:05, junior
+# implementar a associacao do argumento com essa tarefa
 
+    filetemp = changecodec(filein,args.convert)
+    filetemp.close()
 
-    # print(csv_file)
+#:FIXME: 27.12.14 23:35:23, junior
+# o csv.reader nao suporta o utf8
+
+    f = open('output-utf8.csv','rb')
+    csv_file = csv.reader(f)
 
     for linha in csv_file:
-        print(linha.decode('latin1').encode('utf8'))
+        print(linha)
     
+    filein.close()
+
+
 if __name__ == "__main__":
     main()
-
